61ab73234ef8a5ec01a525b48df85665d7471e27
TheOverwatcher <graysonbonds@gmail.com>
2021-01-20 11:43:59 -0500

    Add RabbitMQ support.
    RabbitMQ helps send and recieve messages. This facilitates async
    processing and allows Shopify to have a response of 200 while processing
    continues. Use of axios was not asyncronous so it is removed.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 const fs = require('fs');
 const express = require('express');
-const axios = require('axios');
+const amqp = require('amqplib/callback_api');
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/queue', (req, res) => {
-    console.log('New item for the queue.');
-    let data = JSON.stringify(req.body);
-
-    // console.log('Payload: ' + data);
-    res.status(200).send({resp:'Queue request processed.'});
-});
 
 app.post('/test', (req, res) => {
     console.log('Request received.');
@@ -27,15 +19,37 @@ app.post('/test', (req, res) => {
     console.log('Sending response from webhook');
     res.status(200).send({resp:'Request processed'});
 
-    console.log('Formulate POST request');
-    axios.post('http://localhost:8088/queue', data)
-    .then((response) => {
-        console.log(`statusCode ${response.status}`);
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+    console.log('Setup connection to RabbitMQ');
+    try {
+        amqp.connect('amqp://localhost', (error0, connection) => {
+            if(error0) {
+                throw error0;
+            }
 
+            connection.createChannel((error1, channel) => {
+                if(error1) {
+                    throw error1;
+                }
+
+                var queue = 'hello';
+                var message = 'Hello World';
+
+                channel.assertQueue(queue, { durable: false })
+
+                channel.sendToQueue(queue, Buffer.from(message));
+                console.log(' [x] sent %s', message)
+            });
+            // Can this be done in a finally
+            setTimeout(() => {
+                connection.close();
+                process.exit(0);
+            }, 500)
+
+        });
+    }
+    catch (error) {
+        console.log("Error occurred: " + error);
+    }
 });
 
 const server = app.listen(
